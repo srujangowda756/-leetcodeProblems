b4fd321cee1d9f5fc60fd8c87feedee88a70de08
Srujan K <srujangowda756@gmail.com>
2026-07-05 22:16:12 +0530

    Time: 0 ms (100%), Space: 21.1 MB (5.44%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        self.prev=float("-inf")
+        def inorder(node):
+            if not node:
+                return True
+            if not inorder(node.left):
+                return False
+            if node.val<=self.prev:
+                return False
+            self.prev=node.val
+            return inorder(node.right)
+        return inorder(root)
