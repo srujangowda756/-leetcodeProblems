d900ea83ebf0ef20bb4bb23e6758ade67f382932
Srujan K <srujangowda756@gmail.com>
2026-07-23 09:41:08 +0530

    Time: 2 ms (71.75%), Space: 63.4 MB (86.72%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let ans=nums[0]
-    let tot=0
+    let ans = -Infinity
+    let tot = 0
     for(let i=0;i<nums.length;i++){
         tot+=nums[i]
         if(tot>ans){
